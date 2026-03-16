f2e98f49ac8c6af0f9643f0b0b1fc73bfeb89b38
Hamza Anjum <hamzaanjum905@gmail.com>
2026-03-16 22:31:30 +0000

    2nd and 3rd Adts adde (queue and stack) with appropriate methods

--- SecureCycle/src/securecycle/RecoveryHistoryQueue.java
+++ SecureCycle/src/securecycle/RecoveryHistoryQueue.java
@@ -10,4 +10,51 @@ package securecycle;
  */
 public class RecoveryHistoryQueue {
     
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public RecoveryHistoryQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public int size() {
+        return size;
+    }
+    
+    //enqueue method to add a recovered bike to the back of the queue
+    public void enqueue(Object element) {
+        Node newNode = new Node(element);
+        if (isEmpty()) {
+            head = newNode; //if empty, the new node is both head and tail
+        } else {
+            tail.setNext(newNode); //linking the current tail to the new node
+        }
+        tail = newNode; //updating tail pointer to the new node
+        size++;
+    }
+    
+    //dequeue method to remove and return the oldest recovery record from the fornt of the queue
+    public Object dequeue() {
+        if (isEmpty()) {
+            return null;
+        }
+        Object item = head.getElement(); //geting data from the front node
+        head = head.getNext(); //moving head pointer to the next node
+        
+        if (head == null) {
+            tail = null; //if the queue becomes empty, reset tail to null
+        }
+        
+        size--;
+        return item;
+    }
+   
+    
 }
--- SecureCycle/src/securecycle/UrgentAlertStack.java
+++ SecureCycle/src/securecycle/UrgentAlertStack.java
@@ -10,4 +10,49 @@ package securecycle;
  */
 public class UrgentAlertStack {
     
+    private Node head;
+    private int size;
+
+    public UrgentAlertStack() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public int size() {
+        return size;
+    }
+    
+    //push method to add a new stolen bike alert to the top of the stack so teh police can see the latest theft
+    public void push(Object newItem) {
+        Node newNode = new Node(newItem);
+        if (head != null) {
+            newNode.setNext(head); //linking new node to the current top
+        }
+        head = newNode; //making the new node the new top
+        size++;
+    }
+    
+    //pop method to remove and return the most recent alert from the top for the police
+    public Object pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        Object item = head.getElement(); //getting data from the top node
+        head = head.getNext(); //moving head to the next node, removing the top one
+        size--;
+        return item;
+    }
+    
+    //peek method to look at the top alert without removing it
+    public Object peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return head.getElement(); //returning the data at the head
+    }
+    
 }
