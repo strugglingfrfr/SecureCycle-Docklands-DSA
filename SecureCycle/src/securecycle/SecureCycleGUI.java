67b3c4221bb65c5ec773f5b3e7547a7e61dc8e2d
Hamza Anjum <hamzaanjum905@gmail.com>
2026-03-18 00:22:17 +0000

    Main GUI Design and logic added

--- SecureCycle/src/securecycle/SecureCycleGUI.java
+++ SecureCycle/src/securecycle/SecureCycleGUI.java
@@ -10,6 +10,11 @@ package securecycle;
  */
 public class SecureCycleGUI extends javax.swing.JFrame {
     
+    // Initializing the shared data structures here
+    private RegistryList masterRegistry = new RegistryList();
+    private UrgentAlertStack stolenAlerts = new UrgentAlertStack();
+    private RecoveryHistoryQueue recoveryLog = new RecoveryHistoryQueue();
+    
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(SecureCycleGUI.class.getName());
 
     /**
@@ -28,21 +33,118 @@ public class SecureCycleGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        lblHeading = new javax.swing.JLabel();
+        btnResident = new javax.swing.JButton();
+        btnOfficer = new javax.swing.JButton();
+        lblSubheading = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(245, 245, 250));
+
+        jPanel1.setBackground(new java.awt.Color(0, 51, 102));
+
+        lblHeading.setFont(new java.awt.Font("Arial Black", 1, 36)); // NOI18N
+        lblHeading.setForeground(new java.awt.Color(255, 255, 255));
+        lblHeading.setText("SECURECYCLE");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(33, Short.MAX_VALUE)
+                .addComponent(lblHeading, javax.swing.GroupLayout.PREFERRED_SIZE, 678, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblHeading, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        btnResident.setBackground(new java.awt.Color(70, 130, 180));
+        btnResident.setFont(new java.awt.Font("Arial Black", 1, 14)); // NOI18N
+        btnResident.setForeground(new java.awt.Color(255, 255, 255));
+        btnResident.setText("RESIDENT PORTAL");
+        btnResident.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnResidentActionPerformed(evt);
+            }
+        });
+
+        btnOfficer.setBackground(new java.awt.Color(70, 130, 180));
+        btnOfficer.setFont(new java.awt.Font("Arial Black", 1, 14)); // NOI18N
+        btnOfficer.setForeground(new java.awt.Color(255, 255, 255));
+        btnOfficer.setText("OFFICER DASHBOARD");
+        btnOfficer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOfficerActionPerformed(evt);
+            }
+        });
+
+        lblSubheading.setBackground(new java.awt.Color(255, 255, 255));
+        lblSubheading.setFont(new java.awt.Font("Arial Black", 1, 14)); // NOI18N
+        lblSubheading.setForeground(new java.awt.Color(0, 51, 102));
+        lblSubheading.setText("Smart Docklands Bike Security & Recovery Portal");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(96, 96, 96)
+                .addComponent(btnResident, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnOfficer)
+                .addGap(96, 96, 96))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(162, 162, 162)
+                .addComponent(lblSubheading)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblSubheading, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(110, 110, 110)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnResident, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnOfficer, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(97, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnResidentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResidentActionPerformed
+        // TODO add your handling code here:
+        //creating the resident window and passing the shared registry and alerts to it
+        ResidentGUI residentPortal = new ResidentGUI(masterRegistry, stolenAlerts);
+        
+        //showing the resident GUI
+        residentPortal.setVisible(true);
+        
+        //Hiding the main GUI
+        this.setVisible(false);
+    }//GEN-LAST:event_btnResidentActionPerformed
+
+    private void btnOfficerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOfficerActionPerformed
+        // TODO add your handling code here:
+        //creating the officer window and passing the shared registry and alerts to it
+        OfficerGUI officerPortal = new OfficerGUI(stolenAlerts, recoveryLog);
+        
+        //showing the officer GUI
+        officerPortal.setVisible(true);
+        
+        //Hiding the main GUI
+        this.setVisible(false);
+    }//GEN-LAST:event_btnOfficerActionPerformed
 
     /**
      * @param args the command line arguments
@@ -70,5 +172,10 @@ public class SecureCycleGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnOfficer;
+    private javax.swing.JButton btnResident;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblHeading;
+    private javax.swing.JLabel lblSubheading;
     // End of variables declaration//GEN-END:variables
 }
